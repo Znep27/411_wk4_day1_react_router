3b85e0cb4bfecc0fa06e5ff45f2f8558438c57ef
Marc Rozenburgh <znep27@hotmail.com>
2023-11-09 16:20:47 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
+import React from "react";
+import Navigation from "./components/Navigation";
+import "./App.css";
 // Write imports for Router & BrowserRouter here //
+import Router from "./Router";
+// import { BrowserRouter } from "react-router-dom";
+
+// test
+// import Home from "./components/Home";
 
 function App() {
   return (
-    <Navigation />
+    <>
+      <Navigation />
+      <Router />
+    </>
   );
 }
+
+// now render the Router,
 
 export default App;
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,14 +1,62 @@
-import React from 'react'
-import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import React from "react";
+import cars from "../cars.json";
+import { useParams } from "react-router-dom";
 
-// import MUI components here //
-// Container, Paper, Chip //
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
-}
+  const params = useParams();
 
-export default Car
+  const car = cars.find((car) => car.id === +params.id);
+
+  let chips = [];
+  for (let property in car) {
+    if (property === "Name") {
+      let nameArr = car[property].split(" ");
+      nameArr.forEach((element, index, arr) => {
+        arr[index] = element[0].toUpperCase() + element.slice(1);
+      });
+      chips.push(
+        <Chip key={property} label={`${property}: ${nameArr.join(" ")}`} />
+      );
+    } else {
+      chips.push(
+        <Chip key={property} label={`${property}: ${car[property]}`} />
+      );
+    }
+  }
+
+  return (
+    <>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm" sx={{ paddingTop: 10 }}>
+          <Box>
+            <Paper elevation={4} sx={{ padding: "35px" }}>
+              <h1>{car.Name}</h1>
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                  display: "inline",
+                  padding: 1,
+                }}
+              >
+                {chips}
+              </Stack>
+            </Paper>
+          </Box>
+        </Container>
+      </React.Fragment>
+    </>
+  );
+};
+
+export default Car;
